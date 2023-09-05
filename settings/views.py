7a85918a0a814188d074784c2e53182d4a96321a
Mmy2000 <yahianada005@gmail.com>
2023-09-05 07:12:13 +0300

    update 6

--- settings/views.py
+++ settings/views.py
@@ -4,7 +4,7 @@ from django.db.models.query_utils import Q
 # Create your views here.
 def home(request):
     movie_list = All.objects.filter(category__name = 'movie').order_by('-created_at')[:6]
-    movie_list2 = All.objects.filter(category__name = 'movie').order_by('-created_at')[:3]
+    all_list = All.objects.filter(category__name = 'movie').order_by('-created_at')[:3]
     series_list = All.objects.filter(category__name = 'series').order_by('-created_at')[:3]
     cartoon_list = All.objects.filter(category__name = 'cartoon').order_by('-created_at')[:3]
 
@@ -12,7 +12,7 @@ def home(request):
 
     return render(request, 'settings/home.html',{
         'movie_list':movie_list,
-        'movie_list2' : movie_list2,
+        'all_list' : all_list,
         'series_list':series_list,
         'cartoon_list':cartoon_list,
         
