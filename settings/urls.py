48162f0e0dd2ecb7a32777ec7f8ec693039ebfe2
Mmy2000 <yahianada005@gmail.com>
2023-09-13 01:05:22 +0300

    update 9

--- settings/urls.py
+++ settings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . views import home 
+from . views import home , home_search , category_filter
 
 app_name = 'settings'
 
 urlpatterns = [
     path('', home ,name = 'home'),
-
+    path('search' , home_search , name = 'home_search'),
+    path('category/<slug:category>' , category_filter , name = 'category_filter'),
 ]
--- settings/views.py
+++ settings/views.py
@@ -9,6 +9,7 @@ def home(request):
     all_list = All.objects.filter().order_by('-created_at')[:6]
     series_list = All.objects.filter(category__name = 'series').order_by('-created_at')[:6]
     cartoon_list = All.objects.filter(category__name = 'cartoon').order_by('-created_at')[:6]
+    category = Category.objects.all()
     
 
     
@@ -18,7 +19,20 @@ def home(request):
         'all_list' : all_list,
         'series_list':series_list,
         'cartoon_list':cartoon_list,
-        
+        'category' : category ,
     })
+
+def home_search(request):
+    name = request.GET.get('name')
+
+    property_list = All.objects.filter(
+        Q(name__icontains = name)
+    )
+    return render(request , 'settings/home_search.html' , {'property_list':property_list})
+
+def category_filter(request , category):
+    category = Category.objects.get(name = category)
+    property_list = All.objects.filter(category=category)
+    return render(request , 'settings/home_search.html' , {'property_list':property_list})
 
 
