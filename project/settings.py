d3760c59426faebdf5cee195c954659a44589f28
Mmy2000 <yahianada005@gmail.com>
2023-11-29 01:12:33 +0200

    reset password part 4

--- project/settings.py
+++ project/settings.py
@@ -139,7 +139,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'my552915@gmail.com'
+EMAIL_HOST_PASSWORD = 'lqcmfjqzglkizdbf'
