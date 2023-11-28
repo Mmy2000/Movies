5d56f6e05c3dc90a11c623dc66960aa4e9e94b17
Mmy2000 <yahianada005@gmail.com>
2023-11-29 00:57:08 +0200

    reset password

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,14 @@ from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate , login 
 from gendr.models import All
-
+from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.urls import reverse
 
 
 
@@ -19,9 +26,7 @@ def signup(request):
                 user = authenticate(username=usernames,password=passwords)
                 login(request,user)
 
-                return redirect('/accounts/login')
-
-                
+                return redirect('/accounts/login')     
         else:
             form=SignupForm()
 
@@ -33,7 +38,31 @@ def profile(request):
 
     return render(request,'profile/profile.html',{'profile':profile})
 
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email__exact=email)
 
+            # Reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Reset Your Password'
+            message = render_to_string('registration/password_reset_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist!')
+            return redirect(reverse('accounts:forgotPassword'))
+    return render(request, 'registration/password_reset_form.html')
 
 def edit_profile(requset):
     profile = Profile.objects.get(user=requset.user)
