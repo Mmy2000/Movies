3d36fe0adb076dec0562b9a9d1d23acfb49821a9
Mmy2000 <yahianada005@gmail.com>
2023-12-24 00:21:17 +0200

    updating

--- project/settings.py
+++ project/settings.py
@@ -129,7 +129,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"static","staticroot")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
-    "/var/www/static/",
 ]
 
 MEDIA_URL = '/media/'
